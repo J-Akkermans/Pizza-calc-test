146765f4bfb129b259a7f2a176bcdd0dce23bae0
J Akkermans <99044668@mydavinci.nl>
2020-09-11 14:59:43 +0200

    Added pizza calc boilerplate
    Added pizza calc boilerplate

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+const KLEIN = 5; 
+const MIDDEL = 7; 
+const GROOT = 10; 
+const pizzaMeer = "pizza's" 
+
+var countKlein = 0;     
+var countMiddel = 0;    
+var countGroot = 0;     
+
+var clicksPizzas = [0, 0, 0] 
+function pizzaCalc(id) { 
+    clicksPizzas[id] += 1; 
+    if (id === 0) { 
+        countKlein++ 
+        if (countKlein == 2) { 
+            document.getElementById("pizzaKlein").innerHTML = pizzaMeer 
+        }
+        var totaalKlein = countKlein * KLEIN; 
+        document.getElementById("totaalPrijsKlein").innerHTML = totaalKlein + " Euro"; 
+        document.getElementById("totaalKlein").innerHTML = countKlein; 
+    }
+    else if (id === 1) { 
+        countMiddel++ 
+        if (countMiddel == 2) {
+            document.getElementById("pizzaMiddel").innerHTML = pizzaMeer 
+        }
+        var totaalMiddel = countMiddel * MIDDEL;
+        document.getElementById("totaalPrijsMiddel").innerHTML = totaalMiddel + " Euro"; 
+        document.getElementById("totaalMiddel").innerHTML = countMiddel; 
+    }
+    else if (id === 2) { 
+        countGroot++ 
+        if (countGroot == 2) {
+            document.getElementById("pizzaGroot").innerHTML = pizzaMeer 
+        }
+        var totaalGroot = countGroot * GROOT;
+        document.getElementById("totaalPrijsGroot").innerHTML = totaalGroot + " Euro"; 
+        document.getElementById("totaalGroot").innerHTML = countGroot; 
+    }
+
+    var totaal = countKlein * KLEIN + countMiddel * MIDDEL + countGroot * GROOT; 
+    document.getElementById("totaal").innerHTML = totaal + " Euro"; 
+}
